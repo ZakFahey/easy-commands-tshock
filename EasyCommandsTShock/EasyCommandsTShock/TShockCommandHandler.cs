b260b8b8528ef27ffb65c59fbee1d13b352d7873
ZakFahey <z.fahey@gmail.com>
2019-05-09 23:02:44 -0400

    Initial project

---
+++ EasyCommandsTShock/EasyCommandsTShock/TShockCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using EasyCommands;
+using EasyCommands.Defaults;
+using TShockAPI;
+
+
+namespace EasyCommandsTShock
+{
+    public class TShockCommandHandler : CommandHandler<TSPlayer>
+    {
+        protected override void Initialize()
+        {
+            AddParsingRules(typeof(DefaultParsingRules<TSPlayer>));
+            AddParsingRules(typeof(TShockParsingRules<TSPlayer>));
+        }
+
+        protected override void SendFailMessage(TSPlayer sender, string message)
+        {
+            sender.SendErrorMessage(message);
+        }
+    }
+}
---
+++ EasyCommandsTShock/EasyCommandsTShock/TShockParsingRules.cs
@@ -0,0 +1,52 @@
+﻿using TShockAPI;
+using TShockAPI.DB;
+using EasyCommands;
+using TerrariaApi.Server;
+using Terraria;
+using System;
+
+namespace EasyCommandsTShock
+{
+    class TShockParsingRules<TSender> : ParsingRules<TSender>
+    {
+        //TODO: implement
+        //TODO: time of day, team color, item prefix, buff - these are primitive types
+        //Item prefixes go with items. Issue?
+
+        [ParseRule]
+        public TSPlayer ParsePlayer(string arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ParseRule]
+        public User ParseUser(string arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ParseRule]
+        public Item ParseItem(string arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ParseRule]
+        public Group ParseGroup(string arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ParseRule]
+        public NPC ParseNPC(string arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ParseRule]
+        public Region ParseRegion(string arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
