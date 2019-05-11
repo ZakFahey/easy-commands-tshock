155ceece72ea886a2f7586c6f908fdc13e609a40
ZakFahey <z.fahey@gmail.com>
2019-05-11 16:52:28 -0400

    Added easy-tp command

--- EasyCommandsTShock/Example/Commands/ExampleCommands.cs
+++ EasyCommandsTShock/Example/Commands/ExampleCommands.cs
@@ -12,6 +12,8 @@ namespace Example.Commands
 {
     class ExampleCommands : CommandCallbacks<TSPlayer>
     {
+        //TODO: example commands demonstrating subcommands, User parse rule, Item parse rule, Group parse rule, NPC parse rule, Region parse rule, 
+
         [Command("sendsillymessage")]
         [HelpText("Sends a very silly message to another player.")]
         public void SendSillyMessage(TSPlayer player, [AllowSpaces]string message)
@@ -38,6 +40,40 @@ namespace Example.Commands
         public void BroadcastWithColor(Color color, [AllowSpaces]string message)
         {
             TShock.Utils.Broadcast(message, color);
+        }
+        
+        [Command("easy-tp")]
+        [CommandPermissions("tshock.tp.self")]
+        [HelpText("Teleport yourself or some other player to another player.")]
+        public void TP(TSPlayer player, TSPlayer player2 = null)
+        {
+            TSPlayer teleporting, target;
+            if(player2 == null)
+            {
+                teleporting = Sender;
+                target = player;
+            }
+            else
+            {
+                teleporting = player;
+                target = player2;
+
+                if(Sender.HasPermission("tshock.tp.others"))
+                {
+                    Fail("You do not have access to this command.");
+                }
+                if(!teleporting.TPAllow && !Sender.HasPermission(Permissions.tpoverride))
+                {
+                    Fail($"{teleporting} has disabled players from teleporting.");
+                }
+            }
+
+            if(!target.TPAllow && !Sender.HasPermission(Permissions.tpoverride))
+            {
+                Fail($"{target} has disabled players from teleporting.");
+            }
+
+            teleporting.Teleport(target.TPlayer.position.X, target.TPlayer.position.Y);
         }
     }
 }
