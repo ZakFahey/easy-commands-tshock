0f67336c236f5e6fba1d53dd19addc3c461470e3
ZakFahey <z.fahey@gmail.com>
2019-05-16 10:23:40 -0700

    Uploaded Nuget package

--- EasyCommandsTShock/EasyCommandsTShock/Properties/AssemblyInfo.cs
+++ EasyCommandsTShock/EasyCommandsTShock/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EasyCommandsTShock")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EasyCommands for TShock")]
+[assembly: AssemblyDescription("Simplifies writing commands for TShock plugins")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EasyCommandsTShock")]
