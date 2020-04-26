c210c7532a5552af1b24816b4218835aff7def24
Eveldee <eveldee0680@live.fr>
2020-04-26 14:49:52 +0200

    Fix order of execution
    No need to waste time getting permissions if the Server is not allowed to execute this command.

--- EasyCommandsTShock/EasyCommandsTShock/TShockCommandHandler.cs
+++ EasyCommandsTShock/EasyCommandsTShock/TShockCommandHandler.cs
@@ -25,13 +25,13 @@ namespace EasyCommandsTShock
 
         public override void PreCheck(TSPlayer sender, CommandDelegate<TSPlayer> command)
         {
-            CommandPermissions permissions = command.GetCustomAttribute<CommandPermissions>();
-
             // Stop the server from running a command if he's not allowed to, it's allowed by default
             if (command.GetCustomAttribute<AllowServer>()?.Allow == false)
             {
                 Fail("The server doesn't have the permission to execute this command.");
             }
+
+            CommandPermissions permissions = command.GetCustomAttribute<CommandPermissions>();
 
             // Stop a user from running a command or subcommand if they don't have permission to use it
             if(permissions != null && permissions.Permissions.Any(p => !sender.HasPermission(p)))
